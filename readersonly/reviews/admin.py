a8fed8462ee27f3aa28a535e9b1d525621b00089
Onikah <mona.kwamboka@gmail.com>
2016-03-26 11:30:05 +0300

    book+review models modifiable in the admin

--- readersonly/reviews/admin.py
+++ readersonly/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . models import Book, Review
 # Register your models here.
+
+#making book and review model entities modifiable in the admin site. 
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('book', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+
+admin.site.register(Book)
+admin.site.register(Review, ReviewAdmin)
+
