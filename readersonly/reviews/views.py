9f253a23c53caf61d846dcd2773e023ea8ba7f1f
Onikah <mona.kwamboka@gmail.com>
2016-03-27 07:49:09 +0300

    pass a form object with details

--- readersonly/reviews/views.py
+++ readersonly/reviews/views.py
@@ -25,4 +25,27 @@ def book_list(request):
 
 def book_detail(request, book_id):
 	book = get_object_or_404(Book, pk=book_id)
-	return render(request, 'reviews/book_detail.html', {'book':book})
+    form = ReviewForm()
+    return render(request, 'reviews/book_detail.html', {'book': book, 'form': form})
+
+
+def add_review(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.book = book
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('reviews:book_detail', args=(book.id,)))
+
+    return render(request, 'reviews/book_detail.html', {'book': book, 'form': form})
